c9e2a18acca093bbc79261af80827246f37ffc0a
Sarwar Ahammed <110556391+sarwar1ahammed@users.noreply.github.com>
2023-04-05 22:46:21 +0530

    Create Ques05.c

---
+++ Ques05.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+    Find the first occurrence of adjacent duplicate values in the array.
+    Function has to return the value of the element.
+*/
+int main()
+{
+    printf("\n");
+    return 0;
+}
